5f0cb9ced0056461f3d02914e19fb71cba7bdfec
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-09-24 12:22:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const browser = document.getElementById("browser-info");
+browser.textContent  = getBrowserInfo();
+
+function getBrowserInfo(){
+	const browserName = navigator.appName;
+    const browserVersion = navigator.appVersion;
+return `You are using ${browserName} version ${browserVersion}`;
+}
